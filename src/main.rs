0e05136d16ea787418f00ed89690c7bfc17561b4
Joe Hoyle <joehoyle@gmail.com>
2023-06-19 16:23:19 +0200

    More tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use raw_sync::locks::{LockImpl, LockInit, Mutex};
 use std::ptr::slice_from_raw_parts;
-use std::time::{Instant};
+use std::time::Instant;
 use std::{marker::PhantomData, mem::size_of};
 
 use shared_memory::{Shmem, ShmemConf};
@@ -75,8 +75,8 @@ struct SharedMemoryContents<K, V> {
 }
 
 impl<
-        K: PartialEq + serde::Serialize + serde::Deserialize<'static>,
-        V: serde::Serialize + serde::Deserialize<'static>,
+        K: std::fmt::Debug + PartialEq + serde::Serialize + serde::Deserialize<'static>,
+        V: std::fmt::Debug + serde::Serialize + serde::Deserialize<'static>,
     > SharedMemoryContents<K, V>
 {
     fn bucket_iter(&self) -> SharedMemoryHashMapBucketIter<K, V> {
@@ -91,7 +91,7 @@ impl<
         unsafe { (self as *const SharedMemoryContents<K, V>).add(1) as *mut Bucket<K, V> }
     }
 
-    pub fn try_insert(&mut self, key: K, value: V) -> Result<(), Error> {
+    pub fn try_insert(&mut self, key: K, value: V) -> Result<Option<V>, Error> {
         let value_size = bincode::serialized_size(&value).unwrap() as usize;
         let key_size = bincode::serialized_size(&key).unwrap() as usize;
         let bucket = Bucket {
@@ -100,15 +100,21 @@ impl<
             key_size: 0,
             last_accessed: Instant::now(),
         };
-        let ptr = unsafe { self.buckets_ptr().byte_add(self.used) };
-        if size_of::<Bucket<K, V>>() + value_size > self.size {
+        if size_of::<Bucket<K, V>>() + value_size + key_size > self.size {
             return Err(Error::TooLargeError);
         }
 
-        while self.used + size_of::<Bucket<K, V>>() + value_size + key_size > self.size {
-            self.evict();
+        let mut removed = None;
+        while self.used() + size_of::<Bucket<K, V>>() + value_size + key_size > self.size {
+            match self.evict() {
+                Some(item) => removed = Some(item),
+                None => {
+                    return Err(Error::TooLargeError);
+                },
+            }
         }
 
+        let ptr = unsafe { self.buckets_ptr().byte_add(self.used) };
         unsafe {
             core::ptr::copy(&bucket, ptr, 1);
             self.bucket_count += 1;
@@ -117,7 +123,7 @@ impl<
             (*ptr).set_value(value);
             self.used += size_of::<Bucket<K, V>>() + (*ptr).value_size + (*ptr).key_size;
         }
-        Ok(())
+        Ok(removed)
     }
 
     pub fn get(&mut self, key: &K) -> Option<V> {
@@ -159,7 +165,7 @@ impl<
                         std::ptr::copy(next_bucket as *const u8, current_ptr as *mut u8, self.used);
                     }
                 }
-                break;
+                return Some(bucket.get_value());
             }
             current_ptr = bucket.next();
             current_index += 1;
@@ -176,11 +182,11 @@ impl<
     }
 
     pub fn used(&self) -> usize {
-        self.used
+        self.used + size_of::<SharedMemoryContents<K, V>>()
     }
 
     pub fn free(&self) -> usize {
-        self.size - self.used
+        self.size - self.used()
     }
 
     pub fn contains_key(&self, key: &K) -> bool {
@@ -213,14 +219,15 @@ impl<
         }
         oldest_bucket
     }
-    pub fn evict(&mut self) {
+    pub fn evict(&mut self) -> Option<V> {
         let bucket = self.get_lru();
         if let Some(bucket) = bucket {
             // Get around the borrow checker... oh dear.
             let to_remove = bucket as *const Bucket<K, V>;
             drop(bucket);
-            self.remove(&unsafe { &*to_remove }.get_key());
+            return self.remove(&unsafe { &*to_remove }.get_key());
         }
+        None
     }
 }
 
@@ -257,8 +264,8 @@ pub struct SharedMemoryHashMap<K, V> {
 }
 
 impl<
-        K: PartialEq + serde::Serialize + serde::Deserialize<'static>,
-        V: serde::Serialize + serde::Deserialize<'static>,
+        K: std::fmt::Debug + PartialEq + serde::Serialize + serde::Deserialize<'static>,
+        V: std::fmt::Debug + serde::Serialize + serde::Deserialize<'static>,
     > SharedMemoryHashMap<K, V>
 {
     pub fn new(size: usize) -> Result<Self, Error> {
@@ -303,11 +310,14 @@ impl<
         self.lock.lock()
     }
 
-    pub fn insert(&mut self, key: K, value: V) -> Option<()> {
-        self.try_insert(key, value).ok()
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        match self.try_insert(key, value) {
+            Ok(option) => option,
+            Err(_) => None,
+        }
     }
 
-    pub fn try_insert(&mut self, key: K, value: V) -> Result<(), Error> {
+    pub fn try_insert(&mut self, key: K, value: V) -> Result<Option<V>, Error> {
         let lock = self.lock().unwrap();
         let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
         contents.try_insert(key, value)
@@ -373,6 +383,12 @@ impl<
         let lock = self.lock().unwrap();
         let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
         contents.clear()
+    }
+
+    pub fn size_of(key: &K, value: &V) -> usize {
+        let value_size = bincode::serialized_size(value).unwrap() as usize;
+        let key_size = bincode::serialized_size(key).unwrap() as usize;
+        size_of::<Bucket<K, V>>() + value_size + key_size
     }
 }
 
--- tests/main.rs
+++ tests/main.rs
@@ -1,11 +1,12 @@
-use std::mem::size_of;
+use nix::{
+    sys::wait::waitpid,
+    unistd::{fork, ForkResult},
+};
 use std::thread::sleep;
 use std::thread::spawn;
 use std::time::Duration;
 
-use shared_hashmap::Bucket;
 use shared_hashmap::SharedMemoryHashMap;
-use shared_hashmap::Error;
 
 #[test]
 fn test_insert() {
@@ -16,7 +17,6 @@ fn test_insert() {
     assert_eq!(map.get(&2), None);
     map.insert(2, 2);
     assert_eq!(map.get(&2), Some(2));
-
 }
 
 #[test]
@@ -24,6 +24,15 @@ fn test_get() {
     let mut map = SharedMemoryHashMap::new(1024).unwrap();
     map.insert(1, 1);
     assert_eq!(map.get(&1), Some(1));
+}
+
+#[test]
+fn test_get_immutable() {
+    let mut map = SharedMemoryHashMap::new(1024).unwrap();
+    map.insert("Hello", "World".to_owned());
+    let mut value = map.get(&"Hello").unwrap();
+    value.push_str("!");
+    assert_eq!(map.get(&"Hello"), Some("World".to_owned()));
 }
 
 #[test]
@@ -50,6 +59,21 @@ fn test_get_lru() {
 }
 
 #[test]
+fn test_lru_evicted() {
+    let mut map = SharedMemoryHashMap::new(168).unwrap();
+    map.try_insert(1, "Hello").unwrap();
+    assert_eq!(map.get(&1), Some("Hello"));
+    map.try_insert(2, "Hello World").unwrap();
+    assert_eq!(map.get(&1), None);
+    assert_eq!(map.get(&2), Some("Hello World"));
+
+    let mut map = SharedMemoryHashMap::new(1024).unwrap();
+    for i in 0..10000 {
+        map.try_insert(i, "Hello".repeat(10)).unwrap();
+    }
+}
+
+#[test]
 fn test_insert_slice() {
     let mut map = SharedMemoryHashMap::new(1024).unwrap();
     map.insert(1, [1, 2, 3]);
@@ -70,7 +94,6 @@ fn test_insert_string() {
 #[test]
 fn test_insert_too_long() {
     //let mut map = SharedMemoryHashMap::new(128).unwrap();
-
 }
 
 #[test]
@@ -135,9 +158,9 @@ fn test_clone() {
 fn test_used() {
     let mut map = SharedMemoryHashMap::new(1024).unwrap();
     map.insert(1, 1);
-    assert_eq!(map.used(), 104);
+    assert_eq!(map.used(), 128);
     map.insert(2, 2);
-    assert_eq!(map.used(), 144);
+    assert_eq!(map.used(), 168);
 }
 
 #[test]
@@ -169,4 +192,25 @@ fn test_race_condition() {
     });
 
     sleep(Duration::from_millis(100));
+}
+
+#[test]
+fn test_fork() {
+    let mut map = SharedMemoryHashMap::new(1024).unwrap();
+    map.insert(1, 1);
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            map.insert(2, 2);
+            waitpid(child, None).unwrap();
+            assert_eq!(map.get(&3), Some(3));
+        }
+        Ok(ForkResult::Child) => {
+            sleep(Duration::from_millis(10));
+            map.insert(3, 3);
+            assert_eq!(map.get(&2), Some(2));
+        }
+        Err(_) => {
+            dbg!("Fork failed");
+        }
+    }
 }
