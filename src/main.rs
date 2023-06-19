47db5fae9d9b8cb5a256a52591629e992f3b04de
Joe Hoyle <joehoyle@gmail.com>
2023-06-19 11:13:43 +0200

    Add locking

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![feature(pointer_byte_offsets)]
 
-use std::marker::PhantomData;
+use raw_sync::locks::{LockImpl, LockInit, Mutex};
 use std::ptr::slice_from_raw_parts;
-use raw_sync::locks::{Mutex, LockInit, LockImpl};
+use std::{marker::PhantomData, mem::size_of};
 
 use shared_memory::{Shmem, ShmemConf};
 
@@ -20,9 +20,8 @@ impl<K, V: serde::Serialize + serde::Deserialize<'static>> Bucket<K, V> {
             bincode::deserialize(&*slice_from_raw_parts(value, self.value_size)).ok()
         };
     }
-    fn set_value( &mut self, value: V) {
+    fn set_value(&mut self, value: V) {
         let value = bincode::serialize(&value).unwrap();
-        dbg!(value.len());
         self.value_size = value.len();
         unsafe {
             let ptr = (self as *mut Bucket<K, V>).add(1) as *mut u8;
@@ -31,9 +30,13 @@ impl<K, V: serde::Serialize + serde::Deserialize<'static>> Bucket<K, V> {
     }
 }
 
-impl<K, V:> Bucket<K, V> {
+impl<K, V> Bucket<K, V> {
     fn next(&self) -> *mut Bucket<K, V> {
-        unsafe { (self as *const Bucket<K, V>).add(1).byte_add(self.value_size) as *mut Bucket<K, V> }
+        unsafe {
+            (self as *const Bucket<K, V>)
+                .add(1)
+                .byte_add(self.value_size) as *mut Bucket<K, V>
+        }
     }
 }
 
@@ -41,82 +44,24 @@ impl<K, V:> Bucket<K, V> {
 pub enum Error {
     TooLargeError,
 }
-
-pub struct LockGuard(*mut u8);
-
-impl LockGuard {
-    fn new(lock: *mut u8) -> Self {
-        unsafe {
-            while *lock != 0 {
-                std::thread::yield_now();
-            }
-            *lock = 1;
-        }
-        Self(lock)
-    }
-}
-
-impl Drop for LockGuard {
-    fn drop(&mut self) {
-        unsafe { *self.0 = 0 }
-    }
-}
-
-struct SharedMemoryContents {
+struct SharedMemoryContents<K, V> {
     bucket_count: usize,
     used: usize,
+    phantom: PhantomData<(K, V)>,
+    size: usize,
 }
 
-unsafe impl<K: Send, V> Send for SharedMemoryHashMap<K, V> {}
-
-///
-/// The layout of the shared memory is
-/// [lock: u8][bucket_count: usize][used: usize][Bucket][value data][Bucket][value data]...
-pub struct SharedMemoryHashMap<K, V> {
-    shm: Shmem,
-    bucket_count: *mut usize,
-    lock: Box<dyn LockImpl>,
-    used: *mut usize,
-    end: *mut u8,
-    buckets: *mut Bucket<K, V>,
-}
-
-impl<K: PartialEq + std::fmt::Debug, V: serde::Serialize + serde::Deserialize<'static>> std::fmt::Debug for SharedMemoryHashMap<K, V> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.bucket_iter().for_each(|b| {
-            f.debug_struct("Bucket").field("key", &b.key).finish();
-        });
-        f.debug_struct("SharedMemoryHashMap")
-            .field("buckets", &self.buckets)
-            .field("bucket_count", unsafe { &*self.bucket_count } )
-            //.field("lock", &self.lock.)
-            .field("end", &self.end)
-            .finish()
-    }
-}
-
-impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemoryHashMap<K, V> {
-    pub fn new(size: usize) -> Self {
-        let shm_conf = ShmemConf::default().size(size);
-        let shm = shm_conf.create().unwrap();
-        let ptr = shm.as_ptr();
-        let size = shm.len();
-        Self {
-            shm,
-            lock: unsafe { Mutex::new(ptr as *mut u8, ptr as *mut u8).unwrap().0 },
-            bucket_count: unsafe { ptr.add(1) as *mut usize},
-            used: unsafe { (ptr.add(1) as *mut usize).add(1) as *mut usize },
-            buckets: unsafe { (ptr.add(1) as *mut usize).add(2) as *mut Bucket<K, V> },
-            end: unsafe { ptr.add(size) },
-        }
-    }
-
+impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemoryContents<K, V> {
     fn bucket_iter(&self) -> SharedMemoryHashMapIter<K, V> {
         SharedMemoryHashMapIter {
             current: 0,
-            current_ptr: self.buckets,
-            hashmap: self,
+            current_ptr: self.buckets_ptr(),
+            map: self,
         }
+    }
+
+    fn buckets_ptr(&self) -> *mut Bucket<K, V> {
+        unsafe { (self as *const SharedMemoryContents<K, V>).add(1) as *mut Bucket<K, V> }
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<()> {
@@ -130,32 +75,21 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
             phantom: PhantomData::<V>,
             value_size: 0,
         };
+        let ptr = unsafe { self.buckets_ptr().byte_add(self.used) };
 
-        let _lock = self.lock();
-
-        let ptr = unsafe { self.buckets.byte_add(self.used()) };
-        let last = self.bucket_iter().last();
-        let ptr = match last {
-            Some(last) => last.next(),
-            None => self.buckets,
-        };
-        if unsafe { ptr.add(1).byte_add(value_size) as *mut u8 } as usize > self.end as usize {
-            dbg!(last.unwrap().value_size);
-            dbg!(self.end as usize - self.buckets as usize);
-            unsafe { dbg!(value_size, ptr, ptr.add(1).byte_add(value_size) as usize - self.buckets as usize, self.end); }
+        if self.used + size_of::<Bucket<K, V>>() + value_size > self.size {
             return Err(Error::TooLargeError);
         }
         unsafe {
             core::ptr::copy(&bucket, ptr, 1);
-            *self.bucket_count += 1;
+            self.bucket_count += 1;
             (*ptr).set_value(value);
-            *self.used = (*ptr).next() as usize - self.buckets as usize;
+            self.used += size_of::<Bucket<K, V>>() + (*ptr).value_size;
         }
         Ok(())
     }
 
     pub fn get(&mut self, key: &K) -> Option<V> {
-        let _lock = self.lock();
         for bucket in self.bucket_iter() {
             if bucket.key == *key {
                 return bucket.get_value();
@@ -166,10 +100,8 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
 
     pub fn remove(&mut self, key: &K) -> Option<V> {
         let mut current_index = 0;
-        let mut current_ptr = self.buckets;
+        let mut current_ptr = self.buckets_ptr();
         let mut next_bucket: *mut Bucket<K, V> = std::ptr::null_mut();
-
-        let _lock = self.lock();
 
         while current_index < self.len() {
             let bucket = unsafe { &*current_ptr };
@@ -178,15 +110,12 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
                 next_bucket = bucket.next()
             }
             if &bucket.key == key {
+                self.bucket_count -= 1;
+                self.used -= size_of::<Bucket<K, V>>() + bucket.value_size;
                 unsafe {
-                    *self.bucket_count -= 1;
                     // Shift all the next memory back.
                     if !next_bucket.is_null() {
-                        std::ptr::copy(
-                            next_bucket as *const u8,
-                            current_ptr as *mut u8,
-                            self.end as usize - next_bucket as usize,
-                        );
+                        std::ptr::copy(next_bucket as *const u8, current_ptr as *mut u8, self.used);
                     }
                 }
                 break;
@@ -198,7 +127,7 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
     }
 
     pub fn len(&self) -> usize {
-        unsafe { *self.bucket_count }
+        self.bucket_count
     }
 
     pub fn is_empty(&self) -> bool {
@@ -206,15 +135,14 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
     }
 
     pub fn used(&self) -> usize {
-        unsafe { *self.used }
+        self.used
     }
 
     pub fn free(&self) -> usize {
-        unsafe { self.shm.len() - *self.used }
+        self.size - self.used
     }
 
-    pub fn contains_key(&mut self, key: &K) -> bool {
-        let _lock = self.lock();
+    pub fn contains_key(&self, key: &K) -> bool {
         for bucket in self.bucket_iter() {
             if bucket.key == *key {
                 return true;
@@ -224,27 +152,163 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemo
     }
 
     pub fn clear(&mut self) {
-        let lock = self.lock().unwrap();
-        let mut data = unsafe { &mut *(*lock as *mut SharedMemoryContents) };
-        data.bucket_count = 1;
-        unsafe {
-            *self.bucket_count = 0;
+        self.bucket_count = 0;
+    }
+}
+
+pub struct SharedMemoryHashMapIter<'a, K, V> {
+    current: usize,
+    current_ptr: *mut Bucket<K, V>,
+    map: &'a SharedMemoryContents<K, V>,
+}
+
+impl<'a, K, V> Iterator for SharedMemoryHashMapIter<'a, K, V> {
+    type Item = &'a Bucket<K, V>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.map.bucket_count {
+            let item = unsafe { &*self.current_ptr };
+            self.current += 1;
+            self.current_ptr = item.next();
+            Some(item)
+        } else {
+            None
         }
+    }
+}
+
+unsafe impl<K: Send, V> Send for SharedMemoryHashMap<K, V> {}
+
+///
+/// The layout of the shared memory is
+/// [lock: u8][bucket_count: usize][used: usize][Bucket][value data][Bucket][value data]...
+pub struct SharedMemoryHashMap<K, V> {
+    shm: Shmem,
+    lock: Box<dyn LockImpl>,
+    phantom: PhantomData<(K, V)>,
+}
+
+// impl<K: PartialEq + std::fmt::Debug, V: serde::Serialize + serde::Deserialize<'static>> std::fmt::Debug for SharedMemoryHashMap<K, V> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         self.bucket_iter().for_each(|b| {
+//             f.debug_struct("Bucket").field("key", &b.key).finish();
+//         });
+//         f.debug_struct("SharedMemoryHashMap")
+//             .field("buckets", &self.buckets)
+//             .field("bucket_count", unsafe { &*self.bucket_count } )
+//             //.field("lock", &self.lock.)
+//             .field("end", &self.end)
+//             .finish()
+//     }
+// }
+
+impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> SharedMemoryHashMap<K, V> {
+    pub fn new(size: usize) -> Self {
+        let shm_conf = ShmemConf::default().size(size);
+        let shm = shm_conf.create().unwrap();
+        let ptr = shm.as_ptr();
+        let size = shm.len();
+        let hashmap = Self {
+            shm,
+            lock: unsafe {
+                Mutex::new(
+                    ptr as *mut u8,
+                    (ptr as *mut u8).byte_add(Mutex::size_of(Some(ptr))),
+                )
+                .unwrap()
+                .0
+            },
+            phantom: PhantomData::<(K, V)>,
+        };
+
+        // Initialize the hashmap.
+        let contents: SharedMemoryContents<K, V> = SharedMemoryContents {
+            bucket_count: 0,
+            used: 0,
+            phantom: PhantomData,
+            size,
+        };
+        {
+            let lock = hashmap.lock().unwrap();
+            unsafe {
+                core::ptr::copy(&contents, *lock as *mut SharedMemoryContents<K, V>, 1);
+            }
+        }
+
+        hashmap
     }
 
     pub fn lock(&self) -> Result<raw_sync::locks::LockGuard<'_>, Box<dyn std::error::Error>> {
         self.lock.lock()
     }
 
-    pub fn dump_data(&self) -> Vec<u8> {
-        for i in 0..500 {
-            unsafe {
-                dbg!( self.buckets.byte_add(i) as *mut u8, *(self.buckets.byte_add(i) as *mut u8 ));
-            }
-        }
-        unsafe {
-            &*slice_from_raw_parts(self.buckets as _, 20)
-        }.to_vec()
+    // pub fn dump_data(&self) -> Vec<u8> {
+    //     for i in 0..500 {
+    //         unsafe {
+    //             dbg!( self.buckets.byte_add(i) as *mut u8, *(self.buckets.byte_add(i) as *mut u8 ));
+    //         }
+    //     }
+    //     unsafe {
+    //         &*slice_from_raw_parts(self.buckets as _, 20)
+    //     }.to_vec()
+    // }
+
+    pub fn insert(&mut self, key: K, value: V) -> Option<()> {
+        self.try_insert(key, value).ok()
+    }
+
+    pub fn try_insert(&mut self, key: K, value: V) -> Result<(), Error> {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.try_insert(key, value)
+    }
+
+    pub fn get(&mut self, key: &K) -> Option<V> {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.get(key)
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.remove(key)
+    }
+
+    pub fn len(&self) -> usize {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.is_empty()
+    }
+
+    pub fn used(&self) -> usize {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.used()
+    }
+
+    pub fn free(&self) -> usize {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.free()
+    }
+
+    pub fn contains_key(&mut self, key: &K) -> bool {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.contains_key(key)
+    }
+
+    pub fn clear(&mut self) {
+        let lock = self.lock().unwrap();
+        let contents = unsafe { &mut *(*lock as *mut SharedMemoryContents<K, V>) };
+        contents.clear()
     }
 }
 
@@ -255,35 +319,17 @@ impl<K: PartialEq, V: serde::Serialize + serde::Deserialize<'static>> Clone
         let shm_conf = ShmemConf::default().size(self.shm.len());
         let shm = shm_conf.os_id(self.shm.get_os_id()).open().unwrap();
         let ptr = shm.as_ptr();
-        let size = shm.len();
         Self {
             shm,
-            lock: unsafe { Mutex::new(ptr as *mut u8, ptr as *mut u8).unwrap().0 },
-            bucket_count: unsafe { ptr.add(1) as *mut usize},
-            used: unsafe { (ptr.add(1) as *mut usize).add(1) as *mut usize },
-            buckets: unsafe { (ptr.add(1) as *mut usize).add(2) as *mut Bucket<K, V> },
-            end: unsafe { ptr.add(size) },
-        }
-    }
-}
-pub struct SharedMemoryHashMapIter<'a, K, V> {
-    current: usize,
-    current_ptr: *mut Bucket<K, V>,
-    hashmap: &'a SharedMemoryHashMap<K, V>,
-}
-
-impl<'a, K, V> Iterator for SharedMemoryHashMapIter<'a, K, V> {
-    type Item = &'a Bucket<K, V>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current < unsafe { *self.hashmap.bucket_count } {
-            let item = unsafe { &*self.current_ptr };
-            self.current += 1;
-            self.current_ptr = item.next();
-            unsafe { dbg!((*self.current_ptr).value_size) };
-            Some(item)
-        } else {
-            None
+            lock: unsafe {
+                Mutex::from_existing(
+                    ptr as *mut u8,
+                    (ptr as *mut u8).byte_add(Mutex::size_of(Some(ptr))),
+                )
+                .unwrap()
+                .0
+            },
+            phantom: PhantomData::<(K, V)>,
         }
     }
 }
